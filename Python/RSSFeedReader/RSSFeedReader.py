dd85ed6e2a1cd4896cc4188c1b4501c2ef334ddb
JellyStanchina <36483419+JellyStanchina@users.noreply.github.com>
2019-07-25 15:20:03 -0700

    Spec and PY update
    Spec: Environment moved to its own file
    PY: Now STDOUTs RSS XML, with error handling

--- Python/RSSFeedReader/RSSFeedReader.py
+++ Python/RSSFeedReader/RSSFeedReader.py
@@ -1,9 +1,18 @@
 # RSSFeedReader
 
-# Set URL for Feed
+# Requests library used for get()
+import requests
+
+# Set the RSS FeedURL
+URL = 'http://www.motortrend.com/widgetrss/motortrend-stories.xml'
 
 # Request RSS Feed for set URL and collect output
-
-# print response output
-
+try:
+  response = requests.get(URL)
+  response.raise_for_status()
+  response_output = response.content
+  print(response.status_code)
+  print(response_output)
+except Exception as error:
+  print(f'Error ocurred:\n{error}')
 
