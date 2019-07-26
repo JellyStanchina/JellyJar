c79d605c65ec516e1ccdfd9ba951cbe271cf65fa
JellyStanchina <36483419+JellyStanchina@users.noreply.github.com>
2019-07-26 11:19:38 -0700

    STDOUT now title node text

--- Python/RSSFeedReader/RSSFeedReader.py
+++ Python/RSSFeedReader/RSSFeedReader.py
@@ -2,6 +2,7 @@
 
 # Requests library used for get()
 import requests
+import xml.etree.ElementTree as ET
 
 # Set the RSS FeedURL
 URL = 'http://www.motortrend.com/widgetrss/motortrend-stories.xml'
@@ -10,9 +11,19 @@ URL = 'http://www.motortrend.com/widgetrss/motortrend-stories.xml'
 try:
   response = requests.get(URL)
   response.raise_for_status()
-  response_output = response.content
-  print(response.status_code)
-  print(response_output)
+  response_content = response.content
+  # print(response_content)
 except Exception as error:
-  print(f'Error ocurred:\n{error}')
+  print(f'RSS Get Error ocurred:\n{error}')
 
+def child_search(root, tag):
+  for child in root:
+    if child.tag == tag:
+      print(child.text)
+    child_search(child, tag)
+
+try:
+  root = ET.fromstring(response.content)
+  child_search(root , 'title')
+except Exception as error:
+  print(f'Title Node Search Error ocurred:\n{error}')
