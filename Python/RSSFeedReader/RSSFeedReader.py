30d8873ede9a311eaf96bde048ba6c13d42c49c4
JellyStanchina <36483419+JellyStanchina@users.noreply.github.com>
2019-07-29 10:54:14 -0700

    Converted to use urllib.request and exit added for exceptions

--- Python/RSSFeedReader/RSSFeedReader.py
+++ Python/RSSFeedReader/RSSFeedReader.py
@@ -1,7 +1,9 @@
 # RSSFeedReader
 
 # Requests library used for get()
-import requests
+# import requests
+import urllib.request
+
 import xml.etree.ElementTree as ET
 
 # Set the RSS FeedURL
@@ -9,12 +11,16 @@ URL = 'http://www.motortrend.com/widgetrss/motortrend-stories.xml'
 
 # Request RSS Feed for set URL and collect output
 try:
-  response = requests.get(URL)
-  response.raise_for_status()
-  response_content = response.content
-  # print(response_content)
-except Exception as error:
-  print(f'RSS Get Error ocurred:\n{error}')
+  response = urllib.request.urlopen(URL)
+  status_code = response.getcode()
+  if not status_code == 200:
+    print(f'URL Request Open Error: {status_code}')
+    exit()
+  with response as RSSPage:
+    content = RSSPage.read()
+except Exception as excepton:
+  print(f'URL Request Open Exception:\n{excepton}')
+  exit()
 
 def child_search(root, tag):
   for child in root:
@@ -23,7 +29,8 @@ def child_search(root, tag):
     child_search(child, tag)
 
 try:
-  root = ET.fromstring(response.content)
+  root = ET.fromstring(content)
   child_search(root , 'title')
-except Exception as error:
-  print(f'Title Node Search Error ocurred:\n{error}')
+except Exception as exception:
+  print(f'Title Node Search Exception:\n{exception}')
+  exit()
